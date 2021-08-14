0be892ec65ff788fdeb691b0dfbb04994880f140
Nguyễn Bá Tuấn Anh <anhnbt.it@gmail.com>
2021-08-14 11:54:54 +0700

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {CookieService} from "ngx-cookie-service";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'my-second-cookie-project';
+  cookieValue!: string;
+
+  constructor(private cookieService: CookieService) {
+  }
+
+  ngOnInit(): void {
+    const cookieExists: boolean = this.cookieService.check('anhnbt');
+    const expiredDate = new Date();
+    expiredDate.setDate(expiredDate.getTime() + (1000 * 60 * 60)); //1 hour
+    if (!cookieExists) {
+      this.cookieService.set('anhnbt', 'Second AnhNBT.CLUB', expiredDate, '/', '.anhnbt.club');
+    }
+    this.cookieValue = this.cookieService.get('anhnbt');
+    console.log(this.cookieValue);
+  }
+
+  clearCookie(): void {
+    this.cookieService.delete('anhnbt');
+  }
 }
